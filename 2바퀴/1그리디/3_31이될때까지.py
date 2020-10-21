7734e0c6457393579b39836ad418c1e7ab7bc7e9
ljh95 <ilegwmam@gmail.com>
2020-10-21 11:24:33 +0900

    1이될때까지 다시풀어보기 3

---
+++ 2바퀴/1그리디/3_31이될때까지.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+count = 0
+while True:
+    if n == 1:
+        print(count)
+        break
+    
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+    count+=1
